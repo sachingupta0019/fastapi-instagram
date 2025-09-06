eb3006410024caaf986c8c95e1b3ad6cca1eacf3
Sachin Gupta <gupta01010.sachin@gmail.com>
2025-09-06 21:34:40 +0530

    Add New ReactAPP

--- Instagram/database/database.py
+++ Instagram/database/database.py
@@ -1,10 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
+##SQLITE_DATABASE_URI = "sqlite:///./instagram_api.db"
+##engine = create_engine(SQLITE_DATABASE_URI, connect_args= {'check_same_thread' : False})
 
-SQLITE_DATABASE_URI = "sqlite:///./instagram_api.db"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLITE_DATABASE_URI, connect_args= {'check_same_thread' : False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
