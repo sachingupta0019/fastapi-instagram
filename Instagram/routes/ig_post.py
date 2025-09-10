32333c3f8d30dabb07536614b2104ade7c300858
Sachin Gupta <gupta01010.sachin@gmail.com>
2025-09-11 03:17:41 +0530

    Fixing images url issues.

--- Instagram/routes/ig_post.py
+++ Instagram/routes/ig_post.py
@@ -43,7 +43,7 @@ def upload_image(img: UploadFile = File(...), cuurent_user: UserAuthSchema = Dep
     return {
         "filename": filename,
         "url": f"/images/{filename}",  # <-- frontend will prepend BASE_URL
-        "image_url_type": {image_url_types}
+        "image_url_type": [image_url_types]
     }
 
 
