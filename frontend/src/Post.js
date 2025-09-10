4de8c0f2a988e625002ea82f448f67fdd9207bb9
Sachin Gupta <gupta01010.sachin@gmail.com>
2025-09-11 03:34:52 +0530

    use url instead filename.

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -119,7 +119,7 @@ function Post({ post, authToken, authTokenType, username }) {
 
       <img
         className="post_img"
-        src={`${BASE_URL}/images/${post.image}`}
+        src={`${BASE_URL}images/${post.image_url}`}
         
         alt=""
       />
